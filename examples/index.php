37fc2287e85c61317035c4008be55041ae6bf6d8
Ali S <ali.saheli@reconinstruments.com>
2012-06-23 18:54:59 -0700

    adding some text to example page

--- examples/index.php
+++ examples/index.php
@@ -1,7 +1,12 @@
 <?php
-include_once 'basic.php';
+include_once '../auth/basic.php';
 $t = new Basic();
-
 ?>
 <html>
+<head>
+    <title> HTTP Restufull Authentication </title>
+</head>
+<body>
+    <h1>if you can see this, it means you are good to go :D</h1>
+</body>
 </html>
