8abce757615bc0a37a325a75123f637b3a5c8bf4
Ali S <ali.saheli@reconinstruments.com>
2012-06-23 22:58:05 -0700

    rename examples files

--- auth/digest.php
+++ auth/digest.php
@@ -6,10 +6,14 @@
  * @author Ali S
  * @version 1.0
  */
-
-class Digest {
+include_once ('base.php');
+class Digest extends Base {
 
     public function __construct(){
+        $isAuthSet = $this->authHeader();
+        if(empty($isAuthSet)){
+            $this->setAuthHeader("Digest");
+        }
     }
 
 
---
+++ examples/digestIndex.php
@@ -0,0 +1,13 @@
+<?php
+include_once '../auth/digest.php';
+$t = new Digest();
+?>
+<!DOC_TYPE html>
+<html>
+<head>
+    <title> HTTP Restufull Authentication </title>
+</head>
+<body>
+    <h1>if you can see this, it means you are good to go :D</h1>
+</body>
+</html>
